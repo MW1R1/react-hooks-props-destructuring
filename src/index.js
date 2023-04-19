f4c109471145167a9c5244140fce56256aa36b55
MW1R1 <nickmwirigi58@gmail.com>
2023-04-19 11:48:41 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+const user = {
+    n: 'Tyler McGinnis',
+    h: '@tylermcginnis',
+    l: 'Eden, Utah'
+  };
+  const { n: name, h: handle, l: location } = user;
+console.log(name) // Tyler McGinnis
+console.log(handle) // @tylermcginnis
+console.log(location) // Eden, Utah
+
